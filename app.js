d5bcb1b0804c4cf5fcb60e688a3b814421d0e154
ilya <izaluzhnyi@gmail.com>
2022-05-20 17:56:03 +0300

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    // res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
