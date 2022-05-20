509eb790f6564b7fc78e24e592cfae5a41bccc8f
ilya <izaluzhnyi@gmail.com>
2022-05-20 12:50:18 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json({extended: true}))
+app.use('/api/parser', require('./parser'))
+
+const PORT = process.env.PORT ||  5001
+
+async function start() {
+    try {
+        app.listen(PORT, () => {
+            console.log(`app has been started on port ${PORT}...`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
---
+++ parser.js
@@ -0,0 +1,79 @@
+const {Router} = require('express')
+const xl = require('excel4node')
+const router = Router()
+const fs = require('fs')
+const url = require('url')
+
+
+router.post('/', async (req, res) => {
+    try {
+        let {table, planZakupok, iniciator, urFace, data, period} = req.body
+        let arrayNameColumn = ['Объект', 'Подразделение', '№ План Закупок(Заявки)', 'Период', 'Содержание', 'ФИО Инициатора', 'ФИО Исполнителя', 'Дата создания', 'Дата получения в работу', 'Сумма заявки', 'Исполнитель(специалист отдела)', 'Статус заявки(оформляется в ручную)', 'Получена товаров(сумма)', 'Оплачено(сумма)']
+        let arrayNameColumn2 = ['razdely_plana', 'ispolnitel', 'cena']
+
+        let wb = new xl.Workbook()
+        let ws = wb.addWorksheet('Sheet 1');
+
+        arrayNameColumn.forEach((el, index) => {
+            ws.cell(1, index + 1).string(el)
+        })
+        table.forEach((el, index) => {
+            ws.cell(index + 2, 1).string(urFace)
+            ws.cell(index + 2, 2).string(el[arrayNameColumn2[0]])
+            ws.cell(index + 2, 3).string(planZakupok)
+            ws.cell(index + 2, 4).string(period)
+            ws.cell(index + 2, 5).string('Содержание')
+            ws.cell(index + 2, 6).string(iniciator)
+            ws.cell(index + 2, 7).string(el[arrayNameColumn2[1]])
+            ws.cell(index + 2, 8).string(data)
+            ws.cell(index + 2, 9).string('Дата получения в работу')
+            ws.cell(index + 2, 10).string(el[arrayNameColumn2[2]])
+            ws.cell(index + 2, 11).string('Исполнитель')
+            ws.cell(index + 2, 12).string('Статус заявки')
+            ws.cell(index + 2, 13).string('Получено товаров')
+            ws.cell(index + 2, 14).string('Оплачено')
+
+        })
+    let promise = new Promise((resolve,reject)=>{
+
+        wb.write(`Заявка ${planZakupok}.xlsx`,(err,stat)=>{
+            if(err) reject(err)
+            else {
+                setTimeout(() => {
+                    resolve();
+                }, 100)
+            }
+        })
+    })
+        return  promise.then(()=>{
+            return res.json({message:'файл создан'})
+        })
+    } catch (e) {
+        console.log(e, 'error')
+        return res.status(400).json({message: 'bad request'})
+    }
+})
+
+router.get('/download/', async (req, res) => {
+  try {
+      const urlRequest = url.parse(req.url, true)
+      const fileName = `Заявка ${urlRequest.query.filename}.xlsx`
+      const file = __dirname+'\\'+ fileName
+        res.download(file,(err => {
+            if(err)console.log('Ошибка: ',err)
+            else{
+                fs.unlink(file, (err) => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                })
+            }
+        }))
+    } catch (e) {
+        console.log(e, 'error')
+        return res.status(400).json({message: 'bad request'})
+    }
+})
+
+module.exports = router
