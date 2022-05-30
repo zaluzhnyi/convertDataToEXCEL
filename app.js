81d7fdf4dbb4d1400ca7d2b2f6d1c560dc61b2a0
ilya <izaluzhnyi@gmail.com>
2022-05-30 10:19:07 +0300

    'CORS'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(function (req, res, next) {
     adr = req.headers.origin
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 app.use(cors({
